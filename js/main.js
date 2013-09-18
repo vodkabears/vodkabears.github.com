2e45d8e6cc90c12d5c3a52d4b3d442b8d22ee2fe
Ilya <dfrost.00@gmail.com>
2013-09-19 02:59:51 +0400

    turn off font scale in a landscape

--- js/main.js
+++ js/main.js
@@ -11,9 +11,6 @@
     });
 
     $(window).load(function () {
-        setTimeout(function () {
-            window.scrollTo(0, 1);
-        }, 0);
         $(document.getElementById('loadingScreen')).fadeOut();
     });
 })($, ContentSlider);
