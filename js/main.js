da47ec53fbce267f933a41dda574e321665c204c
Ilya <dfrost.00@gmail.com>
2013-09-19 02:53:27 +0400

    viewport update, hiding adressbar

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@
     });
 
     $(window).load(function () {
+        setTimeout(function () {
+            window.scrollTo(0, 1);
+        }, 0);
         $(document.getElementById('loadingScreen')).fadeOut();
     });
 })($, ContentSlider);
