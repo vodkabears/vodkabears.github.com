53c458ce0d7528c2b4d4528817bbcad25e485545
VodkaBears <dfrost.00@gmail.com>
2014-04-05 01:11:05 +0400

    Reupload galereya

--- galereya/js/jquery.galereya.js
+++ galereya/js/jquery.galereya.js
@@ -32,10 +32,8 @@
             slides = [],
             htmlOverflow, bodyOverflow,
             gridW,
-            gridH,
             rowCount = 0,
             cellW = 300,
-            cellH = 300,
             scrollTop,
             loadTimeout,
             currentSlideIndex,
@@ -189,7 +187,6 @@
          */
         var calcParams = function () {
             cellW = $cells.width();
-            cellH = $cells.height();
 
             rowCount = Math.floor(self.width() / (cellW + self.options.spacing));
             if (rowCount < 1) {
@@ -197,7 +194,6 @@
             }
 
             gridW = rowCount * cellW + (rowCount - 1) * self.options.spacing;
-            gridH = Math.ceil($imgs.length / rowCount) * cellH;
         };
 
         /**
@@ -480,7 +476,6 @@
             }
             $currentImg.css('margin-top', ($(window).height() - $currentImg.height()) / 2);
             $grid.width(gridW);
-            $grid.height(gridH);
 
             for (var i = 0, len = visibleCells.length; i < len; i++) {
                 placeCell(visibleCells[i], i);
