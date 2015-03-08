8bcb906b48f6b3d421b6c37ae37030ac809962eb
VodkaBears <dfrost.00@gmail.com>
2015-03-08 17:39:53 +0300

    Add a pulsation to the control

--- interdimensional/src/js/page.js
+++ interdimensional/src/js/page.js
@@ -2,5 +2,20 @@ import 'babelify/polyfill';
 import 'wowjs';
 import 'interdimensional';
 
+var interdimensionalControl;
+
 new WOW().init();
 Interdimensional.charge();
+
+document.addEventListener('interdimensional:charge', function interdimensionalChargeListener() {
+  document.removeEventListener('interdimensional:charge', interdimensionalChargeListener, false);
+
+  interdimensionalControl = document.querySelector('.interdimensional-control');
+  interdimensionalControl.classList.add('pulsatory');
+}, false);
+
+document.addEventListener('interdimensional:jump', function interdimensionalJumpListener() {
+  document.removeEventListener('interdimensional:jump', interdimensionalJumpListener, false);
+
+  interdimensionalControl.classList.remove('pulsatory');
+}, false);
